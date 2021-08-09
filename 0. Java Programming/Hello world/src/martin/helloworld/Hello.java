9e32f268d93b5c5793eaf27828eeec9c329520b8
Aredarn <meszaros.martin986@gmail.com>
2021-08-09 08:04:52 +0200

    Java start

---
+++ 0. Java Programming/Hello world/src/martin/helloworld/Hello.java
@@ -0,0 +1,40 @@
+package martin.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+     //   System.out.println("Hello World!");
+     //   System.out.print("Hello JAVA world!");
+
+        int number = 5;
+        System.out.println(number);
+
+        long myLong = 55;
+        System.out.println(myLong);
+
+        double myDouble = 5.43;
+        System.out.println(myDouble);
+
+        float myFloat = (float) 4.54;
+        System.out.println(myFloat);
+
+        char myChar = 'M';
+        System.out.println(myChar);
+
+        String myName = "Martinez";
+        System.out.println(myName);
+
+        boolean myBool = true;
+        System.out.println(myBool);
+
+
+        //*************************************************************//
+
+        int a = 5;
+        int b = 12;
+        int answer = a + b;
+        System.out.println(answer);
+
+
+
+    }
+}
