536c45fd6ab293b0909cb9cf0c855720036516ea
Aredarn <meszaros.martin986@gmail.com>
2021-08-30 21:47:34 +0200

    OOP_1

---
+++ 0. Java Programming/OOP_1/src/org/martin/oop1/Main.java
@@ -0,0 +1,16 @@
+package org.martin.oop1;
+
+public class Main {
+    public static void main(String[] args) {
+        Phone iPhone = new Phone("iPhone 11", 5,8,8);
+
+
+        System.out.println(iPhone.getName());
+
+        iPhone.playMusic("Vicious Circle");
+
+        Phone pixel = new Phone("Pixel 3", 16);
+
+
+    }
+}
